3a3bc86f5928482dede233f3ab6e996891d2d960
CarlEverest78 <thepythoneer@gmail.com>
2023-09-02 11:04:31 +0200

    debugged console and browser responsiveness

--- src/components/Editor/Ide.jsx
+++ src/components/Editor/Ide.jsx
@@ -29,7 +29,7 @@ export default function Ide({ htmlinput, css, javascript, browserTest }) {
           <Editor lang="css" isExpanded={isExpanded} setIsExpanded={setIsExpanded} srcDoc={srcDoc} setSrcDoc={setSrcDoc} isDark={isDark} color="#FDCA01" />
           <Editor lang="javascript" isExpanded={isExpanded} setIsExpanded={setIsExpanded} srcDoc={srcDoc} setSrcDoc={setSrcDoc} isDark={isDark} color="#FF704E" />
         </div> 
-        <div className="pane" style={(browserSize === "open") && {height:"800px"} || (browserSize === "small") && {height:"400px"} || {}}> 
+        <div className="pane" style={{height: ((browserSize === "open") && "700px") || ((browserSize === "small") && "400px") || ((browserSize === "closed") && "60px")}}> 
           <Browser srcDoc={srcDoc} expandBrowser={expandBrowser} setExpandBrowser={setExpandBrowser} browserSize={browserSize} setBrowserSize={setBrowserSize} />
           <Console expandBrowser={expandBrowser} setExpandBrowser={setExpandBrowser} isDark={isDark} />
         </div>
